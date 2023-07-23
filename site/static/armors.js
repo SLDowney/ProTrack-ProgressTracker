718219dfc075f749ee0ff9f5f38146f48ac45522
SLDowney <44175038+SLDowney@users.noreply.github.com>
2023-07-23 11:21:47 -0230

    updates to armor and nav design

--- site/static/armors.js
+++ site/static/armors.js
@@ -34,4 +34,54 @@ window.addEventListener('DOMContentLoaded', function() {
       xhr.send(data);
     }
   });
-  
+// armors.js
+// armors.js
+window.addEventListener('DOMContentLoaded', function() {
+  // Function to hide all armor upgrade sections
+  function hideAllArmorUpgrades() {
+    document.querySelectorAll('.armor_upgrade').forEach(function(section) {
+      section.style.display = 'none';
+    });
+  }
+
+  function updateArmorSections() {
+    // Use the correct class name for the Great Fairy checkboxes
+    var checkedGreatFairies = document.querySelectorAll('.form-checkbox-input:checked');
+
+    // Hide all armor upgrade sections initially
+    hideAllArmorUpgrades();
+
+    // Show the specific armor upgrade section based on the number of Great Fairies checked
+    if (checkedGreatFairies.length >= 1) {
+      document.querySelectorAll('.level-1').forEach(function(section) {
+        section.style.display = 'block';
+      });
+    }
+    if (checkedGreatFairies.length >= 2) {
+      document.querySelectorAll('.level-2').forEach(function(section) {
+        section.style.display = 'block';
+      });
+    }
+    if (checkedGreatFairies.length >= 3) {
+      document.querySelectorAll('.level-3').forEach(function(section) {
+        section.style.display = 'block';
+      });
+    }
+    if (checkedGreatFairies.length >= 4) {
+      document.querySelectorAll('.level-4').forEach(function(section) {
+        section.style.display = 'block';
+      });
+    }
+  }
+
+  // Add event listeners to the Great Fairy checkboxes
+  document.querySelectorAll('.form-checkbox-input').forEach(function(checkbox) {
+    checkbox.addEventListener('change', updateArmorSections);
+  });
+
+  // Initially hide all armor upgrade sections
+  hideAllArmorUpgrades();
+
+  // Update armor sections when the page loads
+  updateArmorSections();
+});
