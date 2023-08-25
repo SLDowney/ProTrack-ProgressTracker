6ba25f473363d3aeb19e8801d9bc903c092012bf
SLDowney <44175038+SLDowney@users.noreply.github.com>
2023-08-25 18:30:09 -0230

    Added percentages table toggle functionality for mobile

---
+++ site/static/layout.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $("#toggleButton").click(function() {
+        $(".percentage").slideToggle();
+    });
+
+    // Check if the screen width is less than a certain threshold (e.g., 600px)
+    function checkWindowSize() {
+        if ($(window).width() <= 600) {
+            $(".percentage").hide();
+        } else {
+            $(".percentage").show();
+        }
+    }
+
+    // Initially check the window size and adjust the table visibility
+    checkWindowSize();
+
+    // Listen for window resize events
+    $(window).resize(function() {
+        checkWindowSize();
+    });
+});
